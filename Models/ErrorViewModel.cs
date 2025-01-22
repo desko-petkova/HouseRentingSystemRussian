073004bb29b80ed2fe7ef32fc0f3015654f75d51
desko-petkova <desko.petkova@gmail.com>
2025-01-22 11:28:53 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HouseRentingSystemRussian.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
