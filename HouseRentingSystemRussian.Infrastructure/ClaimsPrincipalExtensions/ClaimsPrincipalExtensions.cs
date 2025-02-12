111679d77d5b472b5f132babd10dd7319ebf69f6
desko-petkova <desko.petkova@gmail.com>
2025-02-12 22:15:05 +0200

    ClaimsPrincipal extended

---
+++ HouseRentingSystemRussian.Infrastructure/ClaimsPrincipalExtensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+
+namespace HouseRentingSystemRussian.Infrastructure.ClaimsPrincipalExtensions
+{
+    /// <summary>
+    /// Създаваш статичен клас с разширение (extension method), което добавя нов метод към ClaimsPrincipal.
+
+    public static class ClaimsPrincipalExtensions
+    {
+        /// <summary>
+        /// Разширява обекта ClaimsPrincipal с метода Id().
+        /// <returns>ClaimTypes.NameIdentifier(UserId)</returns>
+        public static string? Id(this ClaimsPrincipal user)
+           => user.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
