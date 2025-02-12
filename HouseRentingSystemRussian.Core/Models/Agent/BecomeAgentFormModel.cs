f1eec8728f8bff0d6aac61495fae59e0b21b8ce3
desko-petkova <desko.petkova@gmail.com>
2025-02-12 12:18:30 +0200

    BecomeAgentFormModel added

---
+++ HouseRentingSystemRussian.Core/Models/Agent/BecomeAgentFormModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystemRussian.Infrastructure.Constants.DataConstant;
+
+namespace HouseRentingSystemRussian.Core.Models.Agent
+{
+    public class BecomeAgentFormModel
+    {
+        [Required]
+        [MaxLength(PhoneNumberMaxLength), MinLength(PhoneNumberMinLength)]
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
