7a6bda73e2da6d91fd0d38cd8bd57d1820cf11e4
desko-petkova <desko.petkova@gmail.com>
2025-02-26 11:44:34 +0200

    all view added

--- HouseRentingSystemRussian.Core/Contracts/IAgentService.cs
+++ HouseRentingSystemRussian.Core/Contracts/IAgentService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HouseRentingSystemRussian.Core.Contracts
+﻿namespace HouseRentingSystemRussian.Core.Contracts
 {
     public interface IAgentService
     {
